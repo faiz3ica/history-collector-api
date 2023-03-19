82b3c771cbb23d9076962607cdbef3afaaf15e14
Mohd Faijan <faiz3ica@gmail.com>
2023-03-19 12:27:06 +0530

    Added initial project

---
+++ src/main/java/com/ica/collector/HistoryCollectorApiApplication.java
@@ -0,0 +1,13 @@
+package com.ica.collector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoryCollectorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoryCollectorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ica/collector/HistoryCollectorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ica.collector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoryCollectorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
